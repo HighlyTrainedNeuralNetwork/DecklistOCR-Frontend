428e1678acea82ec02a7c8295bb68da4d3a6513f
Andrei Klepatch <56235713+HighlyTrainedNeuralNetwork@users.noreply.github.com>
2022-08-27 17:10:03 -0400

    base tailwind styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
 let imageBase64 = null;
 let loaded = false;
+// let loaded = true;
 let executionTime = 0;
+// let executionTime = 7.55;
 let cardsFound = 0;
+// let cardsFound = 74;
 let decklist = "";
+// let decklist = "2 Hall of Storm Giants \n4 Consider \n4 Ledger Shredder \n4 Pieces of the Puzzle \n4 Arclight Phoenix \n4 Treasure Cruise \n4 Spirebluff Canal \n3 Fiery Impulse \n4 Lightning Axe \n";
 function readImage(input) {
     let file = input.target.files[0];
     let reader = new FileReader();
@@ -37,26 +41,21 @@ async function uploadImage() {
 }
 </script>
 
-<h1>Turn MTGO Deck Pictures Into Decklists</h1>
-<input type="file" accept=".jpg, .jpeg, .png" on:change={readImage} />
-<button on:click={uploadImage} disabled={imageBase64 == null}>Scan Image</button>
 
+<h1 class="font-semibold text-4xl relative pb-6 text-center">Turn MTGO Deck Pictures Into Decklists</h1>
+
+<div class="pb-6 text-center">
+  <input type="file" accept=".jpg, .jpeg, .png" on:change={readImage} class="h-10 px-6 font-semibold rounded-md file:bg-gray-300 file:border-1"/>
+  <button on:click={uploadImage} disabled={imageBase64 == null} class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-gray-300 disabled:bg-white">Scan Image</button>
+</div>
 
 {#if loaded}
-  <div class="executationStatus">
-    <h2>Found {cardsFound} cards in {executionTime} seconds</h2>
-  </div>
-  <div contenteditable="true" spellcheck="false" bind:innerHTML="{decklist}" class="decklistOutput">
-  </div>
+    <div class="text-center">
+      <h2>Found {cardsFound} cards in {executionTime} seconds</h2>
+    </div>
+    <div contenteditable="true" spellcheck="false" bind:innerHTML="{decklist}" class="whitespace-pre-wrap pb-6 w-2/5	ml-auto mr-auto">
+    </div>
 {/if}
 
-<p>You can submit feedback and bug reports by 
-  <a href="https://twitter.com/AndreiKlepatch">sending me a direct message on Twitter</a> or <a href="https://github.com/HighlyTrainedNeuralNetwork/DecklistOCR-Frontend/issues/new">opening an issue on GitHub</a>.
-  </p>
-
-<style>
-  .decklistOutput {
-    white-space: pre-wrap;
-  }
-</style>
-
+<p class="text-center absolute inset-x-0 bottom-2 text-center">You can bug reports by <a href="https://github.com/HighlyTrainedNeuralNetwork/DecklistOCR-Frontend/issues/new">opening an issue on GitHub</a>.
+</p>
--- src/routes/scan/+server.js
+++ src/routes/scan/+server.js
@@ -1,5 +1,5 @@
 // #FIRST-ORDER-OF-BUSINESS: recheck API routes lockdown
-// #TODO: styling, exception handling for errors and zero cards found, clean dependencies
+// #TODO: exception handling for errors and zero cards found, clean dependencies
 // #OPTIONAL-TODO: clean debugging outputs, loading bar for 10 sec ETA, option to download/copy to clipboard the output
 
 
