263d029b7d881e335ddb5330c0f7ecebef6b874b
Andrei Klepatch <56235713+HighlyTrainedNeuralNetwork@users.noreply.github.com>
2022-10-04 20:24:29 -0400

    patch scan button disable and progress bar resets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,24 +5,27 @@ const progress = tweened(0, {
 		duration: 9000,
 		easing: cubicOut
 	});
-let input;
+let fileInput;
 let imageBase64 = null;
 let error = "";
 let loading = false;
-
 let executionTime = 0;
 let cardsFound = 0;
 let decklist = "";
+
 function readImage(input) {
-    let file = input.target.files[0];
+  let file = input.target.files[0];
+  imageBase64 = null;
+  if (file) {
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function (e) {
         imageBase64 = e.target.result.split(',')[1];
     }
+  }
 }
 async function uploadImage() {
-    input.value = "";
+    fileInput.value = "";
     loading = true;
     error = "";
     progress.set(1);
@@ -35,8 +38,10 @@ async function uploadImage() {
           'Content-Type': 'application/json'
         }
       });
+      imageBase64 = null;
       if (response.status != 200) {
         error = "Something went unexpectedly wrong."
+        loading = false;
         return error
       }
       const data = await response.text();
@@ -44,17 +49,19 @@ async function uploadImage() {
       cardsFound = data.split("\n")[1].split("\n")[0];
       if (cardsFound == 0) {
         error = "No cards could be found in this image."
+        loading = false;
         return error
       }
       decklist = data.split("\n");
       decklist.splice(0,2);
       decklist = decklist.join("\n");
       loading = false;
+      progress.set(0);
     } catch (error) {
       error = "Something went unexpectedly wrong."
       return error
     }
-}
+} 
 async function copyToClipboard() {
   if (navigator && navigator.clipboard && navigator.clipboard.writeText)
     return navigator.clipboard.writeText(decklist);
@@ -66,8 +73,8 @@ async function copyToClipboard() {
 <h1 class="font-semibold text-4xl relative pb-6 text-center">Turn MTGO Deck Pictures Into Decklists</h1>
 
 <div class="pb-6 text-center">
-  <input type="file" accept=".jpg, .jpeg, .png" on:change={readImage} class="h-10 px-6 font-semibold rounded-md file:bg-gray-300 file:border-1" bind:this={input}/>
-  <button on:click={uploadImage} disabled={imageBase64 == null || input.value == ""} class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-gray-300 disabled:bg-white">Scan Image</button>
+  <input type="file" accept=".jpg, .jpeg, .png" on:change={readImage} class="h-10 px-6 font-semibold rounded-md file:bg-gray-300 file:border-1" bind:this={fileInput}/>
+  <button on:click={uploadImage} disabled={imageBase64 == null} class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-gray-300 disabled:bg-white">Scan Image</button>
 </div>
 
 {#if loading}
